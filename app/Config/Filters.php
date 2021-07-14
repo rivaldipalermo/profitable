656bc34ada41f6666d3c7aa8c599cd52152a4e29
asijkay <asijkay@gmail.com>
2021-07-14 17:26:13 +0700

    tinggal save&update crud blog

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -62,5 +62,8 @@ class Filters extends BaseConfig
 	 *
 	 * @var array
 	 */
-	public $filters = [];
+	public $filters = [
+		'login' => ['before' => ['/admin']],
+		'login' => ['before' => ['/user']],
+	];
 }
