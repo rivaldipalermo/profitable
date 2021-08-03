8b03a0bcfcf541ada74b621aefca7d8b84be4682
unknown <ariffauzan02>
2021-08-03 22:50:47 +0700

    Penambahan Setting Admin & Modals Member

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -1,18 +1,20 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\TransaksiModel;
-use App\Models\UriwayattModel;		
+use App\Models\UriwayattModel;
 use App\Models\BiodataModel;
 use App\Models\KotaModel;
 
 
 class UserController extends BaseController
 {
-	
-	
-	
-	public function __construct(){
+
+
+
+	public function __construct()
+	{
 		$this->transaksiModel = new TransaksiModel();
 		$this->biodataModel = new BiodataModel();
 		$this->kotaModel = new KotaModel();
@@ -21,65 +23,62 @@ class UserController extends BaseController
 
 	public function index()
 	{
-        $data = [
-            'title' => 'Dashboard'
-        ];
+		$data = [
+			'title' => 'Dashboard'
+		];
 		return view('User/u_index', $data);
 	}
 	public function topup()
 	{
-        $data = [
-            'title' => 'topup'
-        ];
+		$data = [
+			'title' => 'topup'
+		];
 		return view('user/topup', $data);
 	}
 	public function action()
 	{
-		if($this->request->getVar('action'))
-		{
+		if ($this->request->getVar('action')) {
 			$action = $this->request->getVar('action');
 
-			if($action == 'get_kota')
-			{
+			if ($action == 'get_kota') {
 				$kotaModel = new kotaModel();
 
 				$kotadata = $kotaModel->where('provinsi_id', $this->request->getVar('provinsi_id'))->findAll();
 
 				echo json_encode($kotadata);
 			}
-
-			
 		}
 	}
 
 	public function biodata()
 	{
 
-        $data = [
-            'title' => 'Biodata',
-			'provinsi' => $this-> biodataModel -> getBiodata(),
-			'kota' => $this -> kotaModel -> getKota()
-        ];
+		$data = [
+			'title' => 'Biodata',
+			'provinsi' => $this->biodataModel->getBiodata(),
+			'kota' => $this->kotaModel->getKota()
+		];
 		return view('User/biodata', $data);
 	}
-	
+
 	public function riwayat_tu()
 	{
 		$page_akhir = $this->request->getVar('page_artikel') ? $this->request->getVar('page_artikel') : 1;
-		
-		$data = [
-            'title' => 'Transaksi',
-			'transaksi' => $this->uriwayattModel->paginate(10,'artikel'),//getRiwayattu(),
-			'pager' => $this->uriwayattModel->pager,  
-            'page_akhir'=> $page_akhir
-        ];
 
-		
+		$data = [
+			'title' => 'Transaksi',
+			'transaksi' => $this->uriwayattModel->paginate(10, 'artikel'), //getRiwayattu(),
+			'pager' => $this->uriwayattModel->pager,
+			'page_akhir' => $page_akhir
+		];
+
+
 
 		return view('user/u_riwayat_trans', $data);
 	}
-	
-	public function getInvoice($id){
+
+	public function getInvoice($id)
+	{
 		$table = $this->transaksiModel->getTransaksi($id);
 		$data = [
 			'title' => 'Invoice Topup',
@@ -89,9 +88,9 @@ class UserController extends BaseController
 	}
 	public function resiko()
 	{
-        $data = [
-            'title' => 'Resiko'
-        ];
+		$data = [
+			'title' => 'Resiko'
+		];
 		return view('User/u_resiko', $data);
 	}
 }
