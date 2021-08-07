0e3fd68ad8f457e0c7e652f1de95bc7652f4efd6
5thphase <ridhme2607@gmail.com>
2021-08-08 03:29:33 +0700

    Ganti Seeder Saldo ke 30 user_id pertama

--- app/Database/Seeds/SaldoSeeder.php
+++ app/Database/Seeds/SaldoSeeder.php
@@ -20,10 +20,10 @@ class SaldoSeeder extends Seeder
 		// 	]
 		//   ];
 		$myTime = new Time('now');
-		$singledata = ['user_id' => '1', 'saldo' => '50000', 'is_verified' => 0, 'created_at' => $myTime, 'updated_at' => $myTime];
+		$singledata = ['user_id' => '1', 'saldo' => 0, 'is_verified' => 0, 'created_at' => $myTime, 'updated_at' => $myTime];
 		array_push($data, $singledata);
-		$userids = array(123, 124, 125, 126, 127);
-		$saldos = array(50000, 150000, 300000, 100000);
+		$userids = array(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30);
+		$saldos = array(0);
 		foreach ($userids as &$ids) {
 			$myTime = new Time('now');
 			$singledata['is_verified'] = 0;
@@ -31,6 +31,15 @@ class SaldoSeeder extends Seeder
 			$singledata['saldo'] = $saldos[array_rand($saldos)];
 			array_push($data, $singledata);
 		}
+		// $userids = array(123, 124, 125, 126, 127);
+		// $saldos = array(50000, 150000, 300000, 100000);
+		// foreach ($userids as &$ids) {
+		// 	$myTime = new Time('now');
+		// 	$singledata['is_verified'] = 0;
+		// 	$singledata['user_id'] = $ids;
+		// 	$singledata['saldo'] = $saldos[array_rand($saldos)];
+		// 	array_push($data, $singledata);
+		// }
 		$this->db->table('Saldo')->insertBatch($data);
 	}
 }
