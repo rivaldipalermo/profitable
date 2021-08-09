4baadfbbc5692e8c4078c6dc1e9f6bb07f091bda
Amalia Sani Oktoviani <57140387+amaliasani@users.noreply.github.com>
2021-08-09 18:08:43 +0700

    add Migration Testimoni

---
+++ app/Database/Migrations/2021-08-09-074209_TestimoniInvestor.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TestimoniInvestor extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'id_testimoni' => [
+				'type'          	=> 'INT',
+				'constraint'     	=> 11,
+				'auto_increment'    => true,
+				'unsigned'			=> true,
+			],
+			'testimoni' => [
+				'type'				=> 'TEXT'
+			],
+			'id_user' => [
+				'type' 				=> 'INT',
+				'constraint' 		=> 7,
+				'unsigned' 			=> true,
+			],
+			'created_at' => [
+				'type' 				=> 'DATETIME',
+				'null' 				=> true,
+			],
+			'updated_at' => [
+				'type' 				=> 'DATETIME',
+				'null' 				=> true,
+			]
+		]);
+		$this->forge->addKey('id_testimoni', true);
+		$this->forge->addForeignKey('id_user', 'users', 'id', 'CASCADE', 'CASCADE');
+		$this->forge->createTable('testimoni_investor', TRUE);
+	}
+
+	public function down()
+	{
+		$this->forge->dropTable('testimoni_investor');
+	}
+}
